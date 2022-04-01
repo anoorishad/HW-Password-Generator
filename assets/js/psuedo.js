4bf04ea3d5c20d081821b9abad5d6d4b13fe7e1a
Andrew Noorishad <anoorishad@me.com>
2022-03-31 18:32:23 -0700

    added click event listener for button

--- assets/js/psuedo.js
+++ assets/js/psuedo.js
@@ -1,1 +1,20 @@
 // Assigning Variables
+
+// grabbing button from html
+var btn = document.querySelector('button');
+
+var lower = [];
+var upper = [];
+var numbers = [];
+var specChars = [];
+var bank = [];
+
+btn.addEventListener('click', generate);
+
+function generate(){
+    console.log("test");
+}
+
+
+
+
